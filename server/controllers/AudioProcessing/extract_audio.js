43dd4dcaf1bcb3ba15cd3a447ffc7b6140ba21f5
Rishabh Agarwal <45998880+RishabhAgarwal-2001@users.noreply.github.com>
2021-03-30 12:38:42 +0530

    Revert "Master"

--- server/controllers/AudioProcessing/extract_audio.js
+++ server/controllers/AudioProcessing/extract_audio.js
@@ -2,48 +2,49 @@ const fs = require('fs');
 const fileInfo = require('file-type');
 const { spawn } = require('child_process');
 const ffmpeg = require('ffmpeg-static');
-const path = require('path');
 
 const tmpPath = process.env.TMP_PATH || '../../tmp';
 
-module.exports =  function(videoFile, jobID) {
+module.exports = function(sourceFilePath, jobID) {
   return new Promise((resolve, reject) => {
-    fs.readFile(videoFile, (error, data) => {
-      if(error) {
-        reject(error);
+    fs.open(sourceFilePath, 'r', function(status, fd){
+      if(status) {
+        // Error Reading the source File
+        // Executing the Reject Callback
+        reject(status.message);
       } else {
-        data = data.slice(0, 100);
-        const fileType = fileInfo.fromBuffer(data);
-        if(fileType!==null && fileType==='wav') {
-          resolve(videoFile);
-        } else {
-          const audioFile = `${tmpPath}/${jobID}.wav`;
-          const args = ['-i', videoFile, '-ac', '1', '-ab', '6400', '-ar', '16000', audioFile, '-y'];
-          let ffmpeg_exec_path = path.dirname(require.resolve("ffmpeg-static/package.json"));
-          ffmpeg_exec_path = path.join(ffmpeg_exec_path, 'ffmpeg.exe');
-          const process = spawn(ffmpeg_exec_path, args);
-          
-          // Setting Up Process Callbacks
+        const buffer = Buffer.from(new Uint8Array(100));
+        fs.read(fd, buffer, 0, 100, 0, function(err, num, buff){
+          // Error Reading the source file
+          // Executing the Reject Callback
+          if(err) {
+            reject(err);
+          } else {
+            const fileInformation = fileInfo.fromBuffer(buff);
+            // If it is already an audio file resolve is called
+            if(fileInformation!==null && fileInformation === 'wav') {
+              resolve(sourceFilePath);
+            } else {
+              // Specifying the Output Destination
+              const outputDestination = `${tmpPath}/${jobID}.wav`;
 
-          process.stdout.on('data', (data) => {
-            if(process.env.VERBOSE_FFMPEG){
-              console.log(`stdout: ${data}`);
+              const args = ['-i', sourceFilePath, '-ac', '1', '-ab', '6400', '-ar', '16000', outputDestination, '-y'];
+              var ffmpeg_path = require.resolve('ffmpeg-static') + '\\..\\ffmpeg.exe';
+
+              // Starting a Sub-Process to Convert Video to Audio
+              const process = spawn(ffmpeg_path, args);
+
+              process.on('close', (code)=>{
+                console.log(`Code = ${code}`);
+                if(code==1) {
+                  reject();
+                } else if(code==0) {
+                  resolve(outputDestination);
+                }
+              });
             }
-          });
-
-          process.stderr.on('data', (data) => {
-            console.log(`stderr: ${data}`);
-          });
-          
-          process.on('close', (code)=>{
-            if(code==1) {
-              reject();
-            } else if(code==0) {
-              resolve(audioFile);
-            }
-          });
-
-        }
+          }
+        });
       }
     });
   });
