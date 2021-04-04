0a2ef02ae12779c8dae0bacfbb179effd14c5939
ankitbhadu <47694983+ankitbhadu@users.noreply.github.com>
2021-04-04 22:33:41 +0530

    Change promises to async/await calls and concurrent execution of requests.

--- server/controllers/AudioTranscription/audio_transcribe.js
+++ server/controllers/AudioTranscription/audio_transcribe.js
@@ -1,5 +1,8 @@
 const debug = require('debug')('bin:lib:transcribe-audio');
+
+
 const fs = require('fs');
+
 require('dotenv').config();
 //const wait = require('./wait');
 const _lodash = require('lodash');
@@ -30,9 +33,9 @@ function splitPhrases(phrase = "", clusterSize = 90){
 
 }
 
-//use google speech to transcribe for upto 4 attempts then throw error
+//use google speech to transcribe 
 
-function transcribeAudio(audioPath, contextPhrase = '', language){
+async function transcribeAudio(audioPath, contextPhrase = '', language){
 	const speech = require('@google-cloud/speech');	
 	// Creates a client
 	const speechClient = new speech.SpeechClient();
@@ -58,43 +61,64 @@ function transcribeAudio(audioPath, contextPhrase = '', language){
 			audio,
 			config,
 		  };
+		var data
 
-		speechClient.recognize(request).then((data) => {
+		try{ data =await speechClient.recognize(request);}
+		catch (e) {
+			console.error(e);
+		} finally {
 			const results = _lodash.get(data[0], 'results', []);
 			const transcription = results
-			  .map(result => result.alternatives[0].transcript)
-			  .join('\n');
-			console.log(`Transcription: ${transcription}`);
-			return transcription
-		  })
-		  .catch(err => {
-			console.error('ERROR:', err);
-		  });
-	
+			.map(result => result.alternatives[0].transcript)
+			.join('\n');
+		//console.log(`Transcription: ${transcription}`);
+		return transcription
+		}
+	 		
+
+
 
 }
 
 //transcibe using function transcribAudio function and returns trnscriptions
 module.exports =
 
-{transcriber(audioPaths, phrase, language = 'en-GB'){
+{
+	async transcriber(audioPaths, phrase, language = 'en-GB'){
 
 
 	if(audioPaths.constructor !== Array){
 		audioPaths = [audioPaths];
 	}
 	console.time('transcribe');
+	
+	var result
+	var result_array = [];
+	//creating tasks from audiofiles to be executed
+	const tasks = audioPaths.map(audioPath => transcribeAudio(audioPath,phrase,language))
+	//call a promise on all and wait untill all calls are completed
 
-	return Promise.all( audioPaths.map(audioPath => { return transcribeAudio(audioPath, phrase, language) } ) )
-		.then(transcriptions => {
-			console.time('transcribe');
-			return transcriptions;
-		})
-		.catch(err => {
-			debug('Transcription error:', err);
-			throw Error('An error occurred in the transcription process');
-		})
-	;
+	var results 
+	try{
+	results=await Promise.all(tasks)
+	}catch{
+		console.error(e);
+	}
+	finally{
+		return results
+	}
+	//below code would call in for loop thus being inefficient
+	//call transcriber on each audio file and push to result array	
+	// for(let audioPath of audioPaths){
+	// try{result = await transcribeAudio(audioPath, phrase, language);}
+	// catch{	console.error(e);}
+	// finally{
+	// result_array.push(result);}
+
+	// }
+
+	//return result_array
+	//return results
 
 }
 
