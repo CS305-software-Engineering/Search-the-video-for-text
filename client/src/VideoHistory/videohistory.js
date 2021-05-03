01c598c1e7634f6ed3c79f6b6dd4fac748bcfbbd
RishabhAgarwal-2001 <45998880+RishabhAgarwal-2001@users.noreply.github.com>
2021-05-03 16:15:24 +0530

    Video History Page

--- client/src/VideoHistory/videohistory.js
+++ client/src/VideoHistory/videohistory.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./videohistory.css";
+const qs = require('querystring');
 export default function VideoHistory() {
     const [videos, setvideos] = useState(null);
     const fetchData = async () => {
@@ -14,29 +15,28 @@ export default function VideoHistory() {
             'https://search-the-video-for-text-soft.herokuapp.com/api/v1/get_my_history'
             , config);
         console.log(response.data);
-        var links = [];
         for(let i=0; i<response.data.length; i++) {
-            console.log("I am inside");
+            console.log(response.data[i].video_id);
             const body = {video_id: response.data[i].video_id};
             const local_config = {
                 headers: config.headers,
                 body: body,
             };
             try{
+                console.log("body");
+                console.log(body);
+                console.log("config");
+                console.log(config);
                 const res = await axios.post(
                     'https://search-the-video-for-text-soft.herokuapp.com/api/v1/get_video_streaming_link',
-                    body,
+                    qs.stringify(body),
                     config
                 );
-                console.log(res.data.video_link);
-                links.push(res.data.video_link);
                 response.data[i].link = res.data.video_link;
             } catch(err) {
                 console.log(err);
             }
         }
-        console.log(response.data);
-        console.log(links);
         setvideos(response.data);
     };
 
@@ -59,17 +59,17 @@ export default function VideoHistory() {
                     videos.map((video, index) => {
                         const cleanedDate = new Date(video.date_created).toDateString();
                         //const authors = video.authors.join(", ");
-
+                        console.log(video.link);
                         return (
                             <div className="video" key={index}>
                                 <h3>video {index + 1}</h3>
                                 <h2>{video.file_name}</h2>
                                 
-                                <iframe
+                                <video
                                     style={{ marginLeft: "20px", marginTop: "20px" }}
                                     width = "60%"
                                     
-                                    src={video.link}
+                                    src={"https://user-upload-videos-iitrpr.s3.us-east-2.amazonaws.com/videos/9c7dc8c4-b9ed-4154-935a-f53c000a1fcc/05b5fcea-8736-4cd0-8499-ab8e2209963c.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAX3SYYYONN5OJIBE5%2F20210503%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20210503T083441Z&X-Amz-Expires=604800&X-Amz-Signature=975252e9d3b5e2e798dece63acaf8ce2d3165748737a17a3344c9c2640de600c&X-Amz-SignedHeaders=host"}
                                     frameBorder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                     allowFullScreen
