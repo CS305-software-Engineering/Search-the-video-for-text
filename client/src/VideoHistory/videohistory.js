c88ceb856f0571a174a8e94c6958e22cbf92f529
RishabhAgarwal-2001 <45998880+RishabhAgarwal-2001@users.noreply.github.com>
2021-05-03 13:39:14 +0530

    Video History Page

--- client/src/VideoHistory/videohistory.js
+++ client/src/VideoHistory/videohistory.js
@@ -3,19 +3,40 @@ import axios from "axios";
 import "./videohistory.css";
 export default function VideoHistory() {
     const [videos, setvideos] = useState(null);
-
     const fetchData = async () => {
         const config = {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI5YzdkYzhjNC1iOWVkLTQxNTQtOTM1YS1mNTNjMDAwYTFmY2MiLCJpYXQiOjE2MTg4NDc3MTAsImV4cCI6MTYxOTQ1MjUxMH0.37Xk5wFQO4TAuxfVqDPME0g42uc9ArtOBJj7sLUUNhU'// + token //the token is a variable which holds the token
+                'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJmYzVkZTYyYi0zMzJhLTQyZTAtOGY1Zi04ODg4MzBlZmFiM2QiLCJpYXQiOjE2MjAwMjc0MTksImV4cCI6MTYyMDYzMjIxOX0.Y5hErwGdCrfeo2hZ0uGl5y1gIz-EYjv-Tu7opDIUFyA'// + token //the token is a variable which holds the token
             }
         }
         const response = await axios.get(
             'https://search-the-video-for-text-soft.herokuapp.com/api/v1/get_my_history'
             , config);
         console.log(response.data);
-
+        var links = [];
+        for(let i=0; i<response.data.length; i++) {
+            console.log("I am inside");
+            const body = {video_id: response.data[i].video_id};
+            const local_config = {
+                headers: config.headers,
+                body: body,
+            };
+            try{
+                const res = await axios.post(
+                    'https://search-the-video-for-text-soft.herokuapp.com/api/v1/get_video_streaming_link',
+                    body,
+                    config
+                );
+                console.log(res.data.video_link);
+                links.push(res.data.video_link);
+                response.data[i].link = res.data.video_link;
+            } catch(err) {
+                console.log(err);
+            }
+        }
+        console.log(response.data);
+        console.log(links);
         setvideos(response.data);
     };
 
@@ -48,7 +69,7 @@ export default function VideoHistory() {
                                     style={{ marginLeft: "20px", marginTop: "20px" }}
                                     width = "60%"
                                     
-                                    src={`https://www.youtube.com/embed/rokGy0huYEA`}
+                                    src={video.link}
                                     frameBorder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                     allowFullScreen
